d0cedb0b9b26e2117ad505bcff14c42182a23068
Goyal <goyaln2@nih.gov>
2019-06-28 13:26:42 -0400

    Added use of instance folder

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg')
 db = SQLAlchemy(app)    #db object represents the database
 migrate = Migrate(app,db)   #object representing migration engine
 
